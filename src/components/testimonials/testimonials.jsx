e9b6d261d3b1dda3e7dd541246801d4a33b4d51a
NateMacLaurin <blackmagenate@gmail.com>
2021-07-28 02:15:21 -0500

    base functionality and responsive design completed

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import "./testimonials.scss";
+import { TestimonialData } from "../../data.js";
 
 export default function Testimonials() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData(TestimonialData);
+    }, []);
+
     return (
         <div className="testimonials" id="testimonials">
-            testimonials
+            <h1>Testimonials</h1>
+            <div className="container">
+                {data.map((cardData) => {
+                    return(
+                        <div className={ cardData.featured? "card featured" : "card"}>
+                            <div className="top">
+                                <img src="assets/right-arrow.png" className="left" alt="Left Arrow" />
+                                <img src={cardData.img} className="user" alt="Right Arrow" />
+                                <img src={cardData.icon} className="right" alt="Right Arrow" />
+                            </div>
+                            <div className="center">
+                                {cardData.desc}
+                            </div>
+                            <div className="bottom">
+                                <h3>{cardData.name}</h3>
+                                <h4>{cardData.title}</h4>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 };
